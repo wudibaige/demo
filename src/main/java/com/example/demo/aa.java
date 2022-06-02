c243b78f4a1655f99e265d617d0ea893ce846185
baige <3279803188@qq.com>
2022-06-02 16:01:59 +0800

    你好啊

--- src/main/java/com/example/demo/aa.java
+++ src/main/java/com/example/demo/aa.java
@@ -3,5 +3,8 @@ package com.example.demo;
 public class aa {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("再见");
+        System.out.println("");
+
     }
 }
