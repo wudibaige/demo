094ef68df72df70f743a9bfe35fabc6276c4c029
baige <3279803188@qq.com>
2022-06-02 16:29:40 +0800

    分支切换

--- src/main/java/com/example/demo/aa.java
+++ src/main/java/com/example/demo/aa.java
@@ -1,10 +1,19 @@
 package com.example.demo;
 
-public class aa {
+import java.util.ArrayList;
+
+public class aa extends Exception{
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("再见");
         System.out.println("你好啊");
-
+        StringBuffer stringBuffer=new StringBuffer("123456");
+        stringBuffer.append("6");
+         String a="s";
+   ArrayList arrayList=new ArrayList();
+   arrayList.add(1,"ss");
+    for(int i=0;i<=2;i++){
+        System.out.println(i);
     }
+     }
 }
